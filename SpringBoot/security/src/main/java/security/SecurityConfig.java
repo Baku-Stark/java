9bc02b3191768ce0db19430b0a04ac753fc4b3e1
Wallace Freitas <103138773+Baku-Stark@users.noreply.github.com>
2025-03-08 05:14:29 -0300

    Spring Security

---
+++ SpringBoot/security/src/main/java/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Libera o Swagger
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
